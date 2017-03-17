c2d7097005a783b55cad7464381952c92d58bbc4
ExploitSori <ljgggg1234@gmail.com>
2017-03-18 02:16:06 +0900

    bug fix

--- parsing.py
+++ parsing.py
@@ -3,30 +3,31 @@ import BeautifulSoup
 import re
 from datetime import datetime
 
-week = ['월', '화', '수', '목', '금', '토', '일']
-school="input your school code"
+week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+school="B100000662"
 
 url="http://stu.sen.go.kr/sts_sci_md00_001.do?schulCode="+school+"&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=1"
 html=urllib.urlopen(url)
 pas=BeautifulSoup.BeautifulSoup(html)
 paslist=pas.findAll('td')
 
-t=datetime.today().weekday()+1#급식 테이블은 첫주 일요일이 0 하지만 weekday 함수는 월요일이 0
-day=datetime.today().day#오늘 날짜 +요일을 더해줘야 테이블의 식단표를 파싱
-
+t=datetime.today().weekday()+1
+day=datetime.today().day
 weeks=day/7
 y=weeks*7
 x=t
 
 result=str(paslist[y+x])
 
-print str(datetime.today().year)+"년 "+str(datetime.today().month)+"월 "+str(day)+"일 "+week[t-1]+"요일"
+print str(datetime.today().year)+"-"+str(datetime.today().month)+"-"+str(day)+"-"+week[t-1]
 
 foodlist=re.split('<br />',result)
-del foodlist[0]
-del foodlist[0]
-
+try:
+    del foodlist[0]
+    del foodlist[0]
+except:
+    print "To day is holyday"
 for i in foodlist:
     food=i
-    sli=food.find('*')#*기준으로 슬라이스
+    sli=food.find('*')
     print food[:sli]
